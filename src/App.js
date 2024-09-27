5a4648a43520246a8ffe3270fcb22de7ba000e9e
hariharasudhanm12 <harim7434@gmail.com>
2024-09-27 12:46:33 +0530

    Updated RecurringDatePicker component and added CSS

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import './App.css';
 import RecurringDatePicker from './components/RecurringDatePicker';
+
 function App() {
   return (
-    <div className="App">
-    <RecurringDatePicker/>
-    </div>
+    <Router>
+      <div className="App">
+        
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/recurring-date-picker">Recurring Date Picker</Link>
+            </li>
+          </ul>
+        </nav>
+
+      
+        <Routes>
+          <Route path="/" element={<h1>Welcome to the Home Page</h1>} />
+          <Route path="/recurring-date-picker" element={<RecurringDatePicker />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
